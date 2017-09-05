ed54d066a6131f1f17b49c78bd75bbf914c1e43d
Alexey Kunzetsov <alexey.kuznetsov@hpe.com>
2017-09-05 17:08:16 +0300

    Initial commit

---
+++ src/ua/org/kuznetsov/Main.java
@@ -0,0 +1,13 @@
+package ua.org.kuznetsov;
+
+//import org.json.simple.JSONObject;
+
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int a=852376413;
+        float b = a/3;
+        System.out.println(b);
+    }
+}
